9ff95b2f6f7169ace0da55e848de29aa57acf90b
Gabriel Ramos <gabriel.ramos@deptagency.com>
2024-09-18 22:52:59 +0200

    feat: Update table border color in Chart component

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -8,24 +8,53 @@ export default function dashboard() {
   return (
     <div className={styles.page}>
       <Header />
+
       <main className={styles.container}>
         <div className={styles.content}>
           <h1>{PageData.title}</h1>
 
-          <Chart
-            title={PageData.overview.total.label}
-            description={PageData.overview.total.description}
-            rows={[
-              { label: "available", value: "30/10" },
-              { label: "percentage", value: "15%" },
-              { label: "ending in 1h", value: "20" },
-              { label: "starting in 1h", value: "5" },
-            ]}
-            style={{
-              backgroundColor: colors.primary,
-              color: colors.white,
-            }}
-          />
+          <section className={styles.charts}>
+            <Chart
+              title={PageData.overview.residents.label}
+              description={PageData.overview.residents.description}
+              rows={[
+                { label: "available", value: "30/10" },
+                { label: "percentage", value: "15%" },
+                { label: "ending in 1h", value: "20" },
+                { label: "starting in 1h", value: "5" },
+              ]}
+            />
+
+            <Chart
+              title={PageData.overview.nonResidentsCars.label}
+              description={PageData.overview.nonResidentsCars.description}
+              rows={[
+                { label: "available", value: "30/10" },
+                { label: "percentage", value: "15%" },
+                { label: "ending in 1h", value: "20" },
+                { label: "starting in 1h", value: "5" },
+              ]}
+              style={{
+                backgroundColor: colors.primary,
+                color: colors.white,
+              }}
+            />
+
+            <Chart
+              title={PageData.overview.nonResidentsMotorcyles.label}
+              description={PageData.overview.nonResidentsMotorcyles.description}
+              rows={[
+                { label: "available", value: "30/10" },
+                { label: "percentage", value: "15%" },
+                { label: "ending in 1h", value: "20" },
+                { label: "starting in 1h", value: "5" },
+              ]}
+              style={{
+                backgroundColor: colors.secondary,
+                color: colors.white,
+              }}
+            />
+          </section>
         </div>
       </main>
     </div>
