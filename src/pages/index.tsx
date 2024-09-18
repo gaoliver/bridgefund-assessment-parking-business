c772e81e8a6e6db510364e09004d2a38444a71a7
Gabriel Ramos <gabriel.ramos@deptagency.com>
2024-09-18 20:04:31 +0200

    feat: Add react-icons package and TextInput component
    - Update package.json to include react-icons as a dependency
    - Add TextInput component to src/components/molecules
    - Create TextInput module CSS file
    - Implement TextInput component functionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { Card } from "@/components/atoms";
+import { TextInput } from "@/components/molecules";
 import styles from "@/styles/login.module.css";
 
 export default function Login() {
@@ -7,7 +8,10 @@ export default function Login() {
       <Card>
         <h1>Login</h1>
 
-        <div className={styles.fields}>email; password;</div>
+        <div className={styles.fields}>
+          <TextInput label="Email" name="email" type="email" placeholder="example@site.com" />
+          <TextInput label="Password" name="password" type="password" />
+        </div>
 
         <div className={styles.ctas}>login button</div>
       </Card>
