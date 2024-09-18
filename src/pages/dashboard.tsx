d7b44d3ecfed3e51cc6fc58cdc0fb5514952175d
Gabriel Ramos <gabriel.ramos@deptagency.com>
2024-09-18 21:36:22 +0200

    refactor: Update dashboard layout and styles

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,12 +1,16 @@
 import { Header } from "@/components/organisms";
 import React from "react";
+import styles from "@/styles/dashboard.module.css";
 
 export default function dashboard() {
   return (
-    <main>
+    <div className={styles.page}>
       <Header />
-      
-      <h1>Dashboard</h1>
-    </main>
+      <main className={styles.container}>
+        <div className={styles.content}>
+          <h1>Dashboard</h1>
+        </div>
+      </main>
+    </div>
   );
 }
