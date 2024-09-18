4af068753cc33cb3b945ab741bbd6f0c2dd529d7
Gabriel Ramos <gabriel.ramos@deptagency.com>
2024-09-19 00:01:15 +0200

    feat: Add color property for Button component

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -5,17 +5,19 @@ export interface ButtonProps extends React.HTMLProps<HTMLButtonElement> {
   children: React.ReactNode;
   variant?: "primary" | "secondary" | "outline" | "ghost" | "link";
   type?: "button" | "submit" | "reset";
+  color?: "danger" | "warning" | "success" | "info";
 }
 
 export const Button: React.FC<ButtonProps> = ({
   children,
   variant = "primary",
   type = "button",
+  color,
   ...props
 }) => {
   return (
     <button
-      className={`${styles.component} ${variant ? styles[variant] : ""}`}
+      className={`${styles.component} ${variant ? styles[variant] : ""} ${color ? styles[color] : ""}`}
       type={type}
       {...props}
     >
