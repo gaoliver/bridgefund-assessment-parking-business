27d08af7c12a98a39a8b178c3173e9197b6e4727
Gabriel Ramos <gabriel.ramos@deptagency.com>
2024-09-18 23:08:54 +0200

    refactor: Set default variant to "primary" in Button component

--- src/components/atoms/Button/Button.tsx
+++ src/components/atoms/Button/Button.tsx
@@ -10,7 +10,7 @@ export interface ButtonProps extends React.HTMLProps<HTMLButtonElement> {
 export const Button: React.FC<ButtonProps> = ({
   children,
   element,
-  variant,
+  variant = "primary",
 }) => {
   const Element = element || "button";
 
