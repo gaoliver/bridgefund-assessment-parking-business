435c4d6a753aa9f9924a989c600565aa4f5fca5d
Gabriel Ramos <gabriel.ramos@deptagency.com>
2024-09-19 16:13:27 +0200

    feat: Add server-side authentication check in login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -6,6 +6,8 @@ import { encrypt } from "@/utils/encrypt";
 import { useFormik } from "formik";
 import { signIn } from "next-auth/react";
 import * as yup from "yup";
+import { GetServerSideProps } from "next";
+import { getSession } from "next-auth/react";
 
 const schema = yup.object().shape({
   email: yup.string().email().required(),
@@ -60,3 +62,20 @@ export default function Login() {
     </main>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context);
+
+  if (session) {
+    return {
+      redirect: {
+        destination: "/dashboard",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {},
+  };
+};
