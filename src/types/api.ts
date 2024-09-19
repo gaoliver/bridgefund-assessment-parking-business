4cefb8c9d76941083203347d02e97d46b3fdf29a
Gabriel Ramos <gabriel.ramos@deptagency.com>
2024-09-19 10:41:30 +0200

    feat: Add api types

---
+++ src/types/api.ts
@@ -0,0 +1,90 @@
+export type LoginWithPasswordDto = {
+  email: string;
+  password: string;
+};
+
+export type UserDto = {
+  id: string;
+  email: string;
+};
+
+export type AuthDto = {
+  accessToken: string;
+  expiresIn: number;
+};
+
+export type UserResponse = {
+  user: UserDto;
+  auth: AuthDto;
+};
+
+export enum VehicleType {
+  CAR,
+  MOTOR,
+}
+
+export type StartParkingSessionDto = {
+  vehicleType: VehicleType;
+  isResident: boolean;
+  vehicleLicensePlate: string;
+};
+
+export type StartParkingSessionRequest = {
+  parkingSession: StartParkingSessionDto;
+};
+
+export type ParkingSessionStartedDto = {
+  parkingSessionId: string;
+  parkingSpaceId: number;
+  sessionStartedAt: string;
+  vehicleLicensePlate: string;
+};
+
+export type ParkingSessionStartedResponse = {
+  startedSession: ParkingSessionStartedDto;
+};
+
+export type EndParkingSessionDto = {
+  parkingSessionId: string;
+};
+
+export type EndParkingSessionRequest = {
+  parkingSession: EndParkingSessionDto;
+};
+
+export type ParkingSessionEndedDto = {
+  parkingSpaceId: number;
+  sessionLengthInHoursMinutes: number;
+};
+
+export type ParkingSessionEndedResponse = {
+  endedSession: ParkingSessionEndedDto;
+};
+
+export type ParkingSpaceRowDto = {
+  parkingSpaceId: number;
+  isOccupied: boolean;
+  occupancy: number;
+  capacity: number;
+  vehicleType: VehicleType;
+};
+
+export type ParkingSpaceListResponse = {
+  parkingSpaces: ParkingSpaceRowDto[];
+};
+
+export type ParkingSessionRowDto = {
+  parkingSessionId: string;
+  parkingSpaceId: number;
+  isSessionEnded: boolean;
+  sessionLengthInHoursMinutes: number;
+  sessionStartedAt: string;
+  sessionEndedAt: string;
+  vehicleLicensePlate: string;
+  vehicleType: VehicleType;
+};
+
+export type ParkingSessionsListResponse = {
+  parkingSessions: ParkingSessionRowDto[];
+  parkingSessionsTotalCount: number;
+};
