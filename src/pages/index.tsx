fd6fcd61676cf700938379a50abf1b7b2c24e4ae
Gabriel Ramos <gabriel.ramos@deptagency.com>
2024-09-20 09:49:05 +0200

    refactor: Add server-side authentication to index page to redirect logic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,26 @@
+import { GetServerSideProps } from "next";
+import { getSession } from "next-auth/react";
+
 export default function Login() {
   return <main></main>;
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const session = await getSession(context);
+
+  if (session) {
+    return {
+      redirect: {
+        destination: "/dashboard",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    redirect: {
+      destination: "/login",
+      permanent: false,
+    },
+  };
+};
